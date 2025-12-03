655ecab57827f1ebd9ebb76694b7f2cce2ee215d
negi2331026 <167751357+negi2331026@users.noreply.github.com>
2025-12-03 16:31:15 +0800

    Update Sniffles2_run.sh

--- analysis/other tools/Sniffles2/Sniffles2_run.sh
+++ analysis/other tools/Sniffles2/Sniffles2_run.sh
@@ -1,4 +1,4 @@
 sniffles --input {tumorBam} --snf {tumor.snf} -t {threads} --minsupport 3 --mapq 10 --min-alignment-length 1000 --output-rnames --allow-overwrite --long-ins-length 100000 --reference {reference}  
 sniffles --input {controlBam} --snf {control.snf} -t {threads} --minsupport 3 --mapq 10 --min-alignment-length 1000 --output-rnames --allow-overwrite --long-ins-length 100000 --reference {reference} 
 sniffles --input {tumor.snf} {control.snf} --vcf {somatic_and_normal_sv.vcf} -t {threads} --minsupport 3 --mapq 10 --min-alignment-length 1000 --output-rnames --allow-overwrite --long-ins-length 100000 --reference {reference}
-python somatic_sv_extract.py -i {somatic_and_normal_sv.vcf} -o {somatic_sv.vcf}
+python extract_somatic_sv.py -i {somatic_and_normal_sv.vcf} -o {somatic_sv.vcf}
