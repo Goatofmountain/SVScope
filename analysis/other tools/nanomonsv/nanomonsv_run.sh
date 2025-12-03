18151e5d61bc481658daafb2380db85ffbf49469
negi2331026 <167751357+negi2331026@users.noreply.github.com>
2025-12-03 16:39:45 +0800

    Create nanomonsv_run.sh

---
+++ analysis/other tools/nanomonsv/nanomonsv_run.sh
@@ -0,0 +1,20 @@
+### ont data:
+
+#! /bin/bash
+export PATH=/home/user/miniconda3/envs/nanomonsv/bin:$PATH 
+nanomonsv parse {tumorBam} {tumorSaveDir}  
+nanomonsv parse {controlBam} {controlSaveDir} 
+nanomonsv get {tumorSaveDir} {tumorBam} {reference}  --control_prefix {controlSaveDir} 
+--control_bam {controlBam} --single_bnd --use_racon --processes {threads} 
+--control_panel_prefix ./hprc_year1_data_freeze_nanopore_guppy4_minimap2_2_24_merge_control_GRCh38
+
+
+### PacBio data:
+
+#! /bin/bash
+export PATH=/home/user/miniconda3/envs/nanomonsv/bin:$PATH 
+nanomonsv parse {tumorBam} {tumorSaveDir}  
+nanomonsv parse {controlBam} {controlSaveDir} 
+nanomonsv get {tumorSaveDir} {tumorBam} {reference}  --control_prefix {controlSaveDir} 
+--control_bam {controlBam} --single_bnd --use_racon --processes {threads} 
+--control_panel_prefix ./hprc_year1_data_freeze_PacBio_HiFi_minimap2_2_24_merge_control_GRCh38
