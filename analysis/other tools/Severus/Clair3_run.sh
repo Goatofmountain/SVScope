8e3a4957e9d76c3661f7a0faa473d2e584b031e8
negi2331026 <167751357+negi2331026@users.noreply.github.com>
2025-12-03 16:55:57 +0800

    Update Clair3_run.sh

--- analysis/other tools/Severus/Clair3_run.sh
+++ analysis/other tools/Severus/Clair3_run.sh
@@ -2,7 +2,7 @@
 
 #! /bin/bash
 export PATH=/home/user/anaconda3/envs/clair3/bin:$PATH 
-{Clair3_path}/run_clair3.sh --bam_fn={controlBam} --ref_fn={reference} --threads={threads} --platform="hifi" --model_path={Clair3_path}/hifi_revio --output={saveDir}  --enable_phasing --longphase_for_phasing 
+{Clair3_path}/run_clair3.sh --bam_fn={controlBam} --ref_fn={reference} --threads={threads} --platform="ont" --model_path={Clair3_path}/r941_prom_hac_g238 --output={saveDir}  --enable_phasing --longphase_for_phasing 
 
 
 
