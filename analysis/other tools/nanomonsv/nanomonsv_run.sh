e7f5322eaf2b40f63989c3d769acf5a13051cd99
negi2331026 <167751357+negi2331026@users.noreply.github.com>
2025-12-03 16:42:34 +0800

    Update nanomonsv_run.sh

--- analysis/other tools/nanomonsv/nanomonsv_run.sh
+++ analysis/other tools/nanomonsv/nanomonsv_run.sh
@@ -4,9 +4,7 @@
 export PATH=/home/user/miniconda3/envs/nanomonsv/bin:$PATH 
 nanomonsv parse {tumorBam} {tumorSaveDir}  
 nanomonsv parse {controlBam} {controlSaveDir} 
-nanomonsv get {tumorSaveDir} {tumorBam} {reference}  --control_prefix {controlSaveDir} 
---control_bam {controlBam} --single_bnd --use_racon --processes {threads} 
---control_panel_prefix ./hprc_year1_data_freeze_nanopore_guppy4_minimap2_2_24_merge_control_GRCh38
+nanomonsv get {tumorSaveDir} {tumorBam} {reference} --control_prefix {controlSaveDir} --control_bam {controlBam} --single_bnd --use_racon --processes {threads} --control_panel_prefix {nanomonsv_data_path}/hprc_year1_data_freeze_nanopore_guppy4_minimap2_2_24_merge_control_GRCh38
 
 
 ### PacBio data:
@@ -15,6 +13,4 @@ nanomonsv get {tumorSaveDir} {tumorBam} {reference}  --control_prefix {controlSa
 export PATH=/home/user/miniconda3/envs/nanomonsv/bin:$PATH 
 nanomonsv parse {tumorBam} {tumorSaveDir}  
 nanomonsv parse {controlBam} {controlSaveDir} 
-nanomonsv get {tumorSaveDir} {tumorBam} {reference}  --control_prefix {controlSaveDir} 
---control_bam {controlBam} --single_bnd --use_racon --processes {threads} 
---control_panel_prefix ./hprc_year1_data_freeze_PacBio_HiFi_minimap2_2_24_merge_control_GRCh38
+nanomonsv get {tumorSaveDir} {tumorBam} {reference} --control_prefix {controlSaveDir} --control_bam {controlBam} --single_bnd --use_racon --processes {threads} --control_panel_prefix {nanomonsv_data_path}/hprc_year1_data_freeze_PacBio_HiFi_minimap2_2_24_merge_control_GRCh38
